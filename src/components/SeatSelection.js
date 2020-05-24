142fe6746bb5e1169925d69dc9442e1aa6b445eb
Bhavna Jain <bhavnajain_16@hotmail.com>
2020-05-24 19:30:48 +0530

    [UPDATE] Css and Layout Changes

--- src/components/SeatSelection.js
+++ src/components/SeatSelection.js
@@ -1,10 +1,45 @@
 import React from 'react';
 
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import Seats from './seats.json';
+
 export default class SeatSelection extends React.Component { 
+  constructor(props) {
+    super(props);
+    this.state = {
+      seatsArray: Seats,
+    }
+  }
+
+  componentDidMount() {
+    console.log('in comp = ', this.state);
+  }
+
   render() {
     return (
       <div>
         <h1> Hello React</h1>
+
+        <Container>
+          <Row>
+            <Col sm="2">
+              YOU
+            </Col>
+          </Row>
+        </Container>
+
+        {/* {
+          this.state.seatsArray && this.state.seatsArray.length ?
+            this.state.seatsArray.map((seat, index) => {
+              <div key={index}>
+                { seat.tname} 
+              </div>
+            })
+          : null
+        } */}
       </div>
     )
   }
