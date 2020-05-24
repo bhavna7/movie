184aeafc5610df14609ff8187d233ddf74637a0d
Bhavna Jain <bhavnajain_16@hotmail.com>
2020-05-24 19:45:56 +0530

    [ADD] Initial Layout for seat selection.

--- src/components/SeatSelection.js
+++ src/components/SeatSelection.js
@@ -4,6 +4,8 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
+import '../App.css'
+
 import Seats from './seats.json';
 
 export default class SeatSelection extends React.Component { 
@@ -11,6 +13,7 @@ export default class SeatSelection extends React.Component {
     super(props);
     this.state = {
       seatsArray: Seats,
+      dummyArray: [1,2,3,4,5,6,7,8,9,20]
     }
   }
 
@@ -21,25 +24,41 @@ export default class SeatSelection extends React.Component {
   render() {
     return (
       <div>
-        <h1> Hello React</h1>
+        <h1>Movie Seat Selection</h1>
 
-        <Container>
+        <Container fluid="true">
+          <Row className="mt-2rem">
+            <h4>Gold Row</h4>
+          </Row>
+
           <Row>
-            <Col sm="2">
-              YOU
-            </Col>
+            {
+              this.state.seatsArray && this.state.seatsArray.length ?
+                this.state.seatsArray.map((data, index) => {
+                  return <Col sm="2" key={index+'____Bhavna'}>
+                    {data.tname}
+                  </Col>
+                })
+              : null
+            }
+          </Row>
+
+          <Row className="mt-2rem">
+            <h4>Diamond Row</h4>
+          </Row>
+
+          <Row>
+            {
+              this.state.seatsArray && this.state.seatsArray.length ?
+                this.state.seatsArray.map((data, index) => {
+                  return <Col sm="2" key={index+'____Bhavna'}>
+                    {data.tname}
+                  </Col>
+                })
+              : null
+              }
           </Row>
         </Container>
-
-        {/* {
-          this.state.seatsArray && this.state.seatsArray.length ?
-            this.state.seatsArray.map((seat, index) => {
-              <div key={index}>
-                { seat.tname} 
-              </div>
-            })
-          : null
-        } */}
       </div>
     )
   }
